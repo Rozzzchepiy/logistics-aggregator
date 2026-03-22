069694cfd41780876862bcea3e63cbf1b538c43a
Rozzzchepiy <rozchepijyura@gmail.com>
2026-03-22 21:28:46 +0200

    implemented driverService

--- logistics-aggregator/src/main/java/rozchepiy/dev/logisticsaggregator/service/DriverService.java
+++ logistics-aggregator/src/main/java/rozchepiy/dev/logisticsaggregator/service/DriverService.java
@@ -1,4 +1,12 @@
 package rozchepiy.dev.logisticsaggregator.service;
 
+import rozchepiy.dev.logisticsaggregator.dto.DriverDTO;
+import java.util.List;
+
 public interface DriverService {
+    List<DriverDTO> getAllDrivers();
+    DriverDTO getDriverById(Long id);
+    DriverDTO createDriverProfile(Long userId, DriverDTO driverDTO);
+    DriverDTO updateDriverProfile(Long id, DriverDTO driverDTO);
+    void deleteDriverProfile(Long id);
 }
